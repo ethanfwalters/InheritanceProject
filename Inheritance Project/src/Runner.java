f2c956df125feef6d86b955c6e3cbce4bac3443b
ethanwalters <ethanwalters@192.168.15.177>
2017-12-19 20:47:30 -0700

    done?

--- Inheritance Project/src/Runner.java
+++ Inheritance Project/src/Runner.java
@@ -6,7 +6,11 @@ public class Runner
 	public static void main(String[] args) 
 	{
 		ArrayList <Nonfiction> adultLibrary = new ArrayList <Nonfiction> ( );
+		
+		ArrayList <Fiction> childLibrary = new ArrayList <Fiction> ( );
+		
 	
+		adultLibrary.add(new Art (1 , "Surrealism"));
 	
 	}
 	
